f4bac6522d0afba6258aa8a2f5d4d0428b3f5149
CantarinoG <guilhermecantarino1709@gmail.com>
2023-01-23 10:19:55 -0300

    Implement shopping cart context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Button, Nav, Container, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 function Navbar() {
+
+    const { openCart, cartQuantity } = useShoppingCart();
 
     return (
     <NavbarBs className="bg-white shadow-sm mb-3" sticky="top">
@@ -11,10 +14,10 @@ function Navbar() {
                 <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
                 <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
             </Nav>
-            <Button  style={{ position: "relative" }}variant="outline-primary" className="rounded-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="currentColor"><path d="M14.35 43.95q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm20 0q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm-22.6-33 5.5 11.4h14.4l6.25-11.4Zm-1.5-3H39.7q1.15 0 1.75 1.05.6 1.05 0 2.1L34.7 23.25q-.55.95-1.425 1.525t-1.925.575H16.2l-2.8 5.2h24.55v3h-24.1q-2.1 0-3.025-1.4-.925-1.4.025-3.15l3.2-5.9L6.45 7h-3.9V4H8.4Zm7 14.4h14.4Z"/></svg>
-            <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)"}}>3</div>
-            </Button>
+            {cartQuantity > 0 && (<Button onClick={openCart} style={{ position: "relative", fontWeight: "bold" }}variant="outline-primary" className="rounded">
+            Cart
+            <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)"}}>{cartQuantity}</div>
+            </Button>)}
         </Container>
     </NavbarBs>
     )
