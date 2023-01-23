00c70b9562bdfa8b4f466f4588797e86b208953a
CantarinoG <guilhermecantarino1709@gmail.com>
2023-01-23 13:41:41 -0300

    Update sample products data

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -15,7 +15,7 @@ function StoreItem({id, name, price, imgUrl}:StoreItemProps) {
     const quantity:number = getItemQuantity(id);
     return <>
         <Card className="h-100">
-            <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit: "cover"}} />
+            <Card.Img variant="top" src={imgUrl} height="400px" style={{objectFit: "cover"}} />
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-between align-items-baselina mb-4">
                     <span className="fs-2">{name}</span>
